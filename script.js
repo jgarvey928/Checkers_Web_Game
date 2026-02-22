07ff4973fe62eb26f11d5ea690e54ba4bed73ecd
jgarvey928 <johngarvey1989@gmail.com>
2026-02-21 19:32:00 -0700

    Update- Added Contact Icons

--- script.js
+++ script.js
@@ -28,6 +28,39 @@ while (container.parentNode && container.parentNode !== document.body) {
 }
 document.body.insertBefore(singlePlayerButton, container);
 
+const socialContainer = document.createElement('div');
+socialContainer.style.cssText = "position: absolute; top: 20px; right: 20px; z-index: 100;";
+
+const createIcon = (url, pathData) => {
+    const link = document.createElement('a');
+    link.href = url;
+    link.target = "_blank";
+    link.style.cssText = "display: inline-block; margin: 0 10px; color: black; opacity: 0.6; transition: opacity 0.2s; vertical-align: middle;";
+    
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("viewBox", "0 0 24 24");
+    svg.setAttribute("width", "24");
+    svg.setAttribute("height", "24");
+    svg.style.fill = "currentColor";
+    
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", pathData);
+    
+    svg.appendChild(path);
+    link.appendChild(svg);
+    
+    link.addEventListener('mouseover', () => link.style.opacity = "1");
+    link.addEventListener('mouseout', () => link.style.opacity = "0.6");
+    
+    return link;
+};
+
+socialContainer.appendChild(createIcon("https://www.linkedin.com/in/john-s-garvey/", "M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z"));
+socialContainer.appendChild(createIcon("https://github.com/jgarvey928", "M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.419-1.305.763-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"));
+socialContainer.appendChild(createIcon("https://jgarvey928.github.io/jsgarveyportfolio.io/", "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"));
+
+document.body.appendChild(socialContainer);
+
 singlePlayerButton.addEventListener('click', () => {
     isSinglePlayer = !isSinglePlayer;
     singlePlayerButton.textContent = isSinglePlayer ? "Disable Single Player" : "Enable Single Player";
